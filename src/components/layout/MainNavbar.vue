9a8100e9bc072f4653764f6134dcc4c9940a17f1
Ceasar <ceasarkwadwo@gmail.com>
2022-03-12 12:24:57 +0000

    project setup

---
+++ src/components/layout/MainNavbar.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        stickyTop: Boolean,
+        default: true
+    },
+    setup(props){
+
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
