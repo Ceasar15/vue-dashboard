5eb88718504ee0a5a777b7ea38d319db86e92a36
Ceasar <ceasarkwadwo@gmail.com>
2022-03-12 13:47:26 +0000

    project setup

--- src/components/layout/SideNavbar.vue
+++ src/components/layout/SideNavbar.vue
@@ -1,91 +1,33 @@
 <template>
-  <aside
-    :class="[
-      'SideNavbar',
-      'col-12',
-      'col-md-3',
-      'col-lg-2',
-      'px-0',
-      sidebarVisible ? 'open' : '',
-    ]"
-  >
-    <div class="main-navbar">
-      <nav
-        class="
-          navbar
-          align-items-stretch
-          navbar-light
-          bg-white
-          flex-md-nowrap
-          border-bottom
-          p-0
-        "
-      >
-        <a class="navbar-brand w-100 mr-0" href="#" style="line-height: 25px">
-          <div class="d-table m-auto">
-            <img
-              id="main-logo"
-              class="d-inline-block align-top mr-1"
-              style="max-width: 25px"
-              src="@/assets/logo.png"
-              alt="Vue Dashboard"
-            />
-            <span v-if="!hideLogoText" class="d-none d-md-inline ml-1"
-              >Vue Here Dashboard</span
-            >
-          </div>
-        </a>
-        <a
-          class="toggle-sidebar d-sm-inline d-md-none d-lg-none"
-          @click="handleToggleSidebar()"
-        >
-          <i class="material-icons">&#xE5C4;</i>
-        </a>
-      </nav>
-      <nav>
-        <h1>Orders</h1>
-        <h3>Analytics</h3>
-        <h4>Categories</h4>
-      </nav>
-    </div>
+  <aside class="SideNavbar" :class="{ collapse: sidebar.collapse }">
+    <nav class="menu">
+      <menu-list :items="sidebar.menus" active-class/>
+    </nav>
+    <footer class="footer">
+      <a class="toggle icon-before icon-circle-left" title="Toggle navigation menu" @click="toggleCollapse"></a>
+      <router-link class="copyright" :to="{ name: 'about' }" :title="'About ' + sidebar.copyright">&copy; {{ sidebar.copyright }}</router-link>
+    </footer>
   </aside>
-  <h1>Dashboard</h1>
-  <h1>Orders</h1>
-  <h3>Analytics</h3>
-  <h4>Categories</h4>
-  <h5>Collections</h5>
-  <h6>Products</h6>
-  <h7>Employers</h7>
 </template>
 
+
 <script>
+import { mapGetters, mapActions } from 'vuex'
+
 export default {
-  name: "SideNavbar",
-  component: "",
-  props: {
-    /**
-     * Whether to hide the logo text, or not.
-     */
-    hideLogoText: {
-      type: Boolean,
-      default: false,
-    },
-  },
-  data() {
-    return {
-      sidebarVisible: false,
-    };
-  },
-};
+  name: 'SideNavbar',
+  components: "",
+  computed: mapGetters({
+    sidebar: 'SideNavbar'
+  }),
+  methods: mapActions({
+    toggleCollapse: 'toggleSidebarCollapse'
+  })
+}
+
+
 </script>
 
-<style lang="scss">
-.main-sidebar {
-  .item-icon-wrapper {
-    display: inline-block;
-  }
-  .dropdown-menu {
-    display: block;
-  }
-}
+<style >
+
 </style>
