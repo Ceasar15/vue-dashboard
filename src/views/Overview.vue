e23f5452598465d510f2e620d4a6caee02b4a8a2
Ceasar <ceasarkwadwo@gmail.com>
2022-03-12 10:57:53 +0000

    project setup

---
+++ src/views/Overview.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+    <span>Dashboard</span>
+    <h3>Overview</h3>
+    <h4> {{ c_here }}</h4>
+    </div>
+    <div>
+        <h3>Users List</h3>
+    </div>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+    name: 'OverView',
+    setup(){
+      const c_here = ref("Here")
+
+      return {
+          c_here
+      }
+    },
+    data(){
+        return { 
+            o_here: 'here'
+        }
+    },
+}
+
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
