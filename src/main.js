9de95b49003ea7d547f8a6ca349cabf05a03787a
Ceasar <ceasarkwadwo@gmail.com>
2022-03-13 11:13:07 +0000

    page overview and structure, routing

--- src/main.js
+++ src/main.js
@@ -5,5 +5,14 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+Vue.config.productionTip = false;
+Vue.prototype.$eventHub = new Vue();
+
+
 /* eslint-disable no-new */
-createApp(App).mount('#app')
+new Vue({
+    router,
+    render: h => h(App),
+}).$mount('#app');
+  
+// createApp(App).mount('#app')
