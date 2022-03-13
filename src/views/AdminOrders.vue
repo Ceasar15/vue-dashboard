691beeeac0f4ab620f353aa10e24082c77ff4365
Ceasar <ceasarkwadwo@gmail.com>
2022-03-13 19:39:19 +0000

    tailwind css stresss

--- src/views/AdminOrders.vue
+++ src/views/AdminOrders.vue
@@ -1,19 +1,14 @@
 <template>
-  <div >
-    <div class="error">
-      <div>
-        <h2>Admin Orders</h2>
-        <h3>Admin Order Details, Table</h3>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </p>
-      </div>
+  <div>
+    <div class="flex flex-wrap -mx-1 overflow-hidden">
+      <div class="my-1 px-1 w-1/6 overflow-hidden">Column Content</div>
+
+      <div class="my-1 px-1 w-1/6 overflow-hidden">Column Content</div>
+    </div>
+    <div class="flex flex-wrap -mx-1 overflow-hidden">
+      <div class="my-1 px-1 w-1/6 overflow-hidden">Column Content</div>
+
+      <div class="my-1 px-1 w-1/6 overflow-hidden">Column Content</div>
     </div>
   </div>
 </template>
@@ -22,10 +17,13 @@
 <script>
 export default {
   name: "AdminOrders",
-  data () {
+  data() {
     return {
-      'dd': 'ff'
-    }
-  }
+      dd: "ff",
+    };
+  },
 };
 </script>
+<style scoped>
+
+</style>
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,19 +1,8 @@
 <template>
-  <div
-    class="
-      p-10
-      grid grid-cols-1
-      sm:grid-cols-1
-      md:grid-cols-3
-      lg:grid-cols-3
-      xl:grid-cols-3
-      gap-5
-    "
-  >
+  <div class="p-10">
     <!--Card 1-->
-    <div class="max-w-[25%] mt-[40px] rounded overflow-hidden shadow-lg">
+    <div class="max-w-sm mt-[40px] rounded overflow-hidden shadow-lg">
       <div class="mr-[200px] text-gray-900 mb-2">Customers</div>
-
       <div class="mr-[220px] text-xl text mb-2 text-black-900 font-bold">
         45,230
       </div>
@@ -35,39 +24,65 @@
         Since last week
       </div>
     </div>
-    <div class="max-w-[25%] mt-[40px] rounded overflow-hidden shadow-lg">
-      <div class="mr-[200px] text-gray-900 mb-2">Customers</div>
-
-      <div class="mr-[220px] text-xl text mb-2 text-black-900 font-bold">
-        45,230
-      </div>
-      &nbsp;
-      <div
-        class="
-          inline-block
-          text-sm
-          font-semibold
-          text-green-500
-          mb-2
-          mr-[250px]
-        "
-      >
-        <em class="font-bold bx bx-up-arrow-alt"></em>
-        <span>525%</span>
-      </div>
-      <div class="mr-[190px] text-sm font-light-500 text-gray-700 mb-2">
-        Since last week
-      </div>
+    <div>
+      <!-- Card 2 -->
+      <p>Orders</p>
+      <p>45,320</p>
+      <p>1.23%</p>
+      <p>since last month</p>
+    </div>
+    <div>
+      <!-- Card 3 -->
+      <p>Earnings</p>
+      <p>$7,524</p>
+      <p>7.85%</p>
+      <p>since last month</p>
+    </div>
+    <div>
+      <!-- Card 4 -->
+      <p>Growth</p>
+      <p>+35.52</p>
+      <p>3.72%</p>
+      <p>since last month</p>
     </div>
   </div>
 
-  <div>
-    <span>Dashboard</span>
-    <h3>Overview</h3>
-  </div>
-  <div>
-    <h3>Users List</h3>
-  </div>
+<div>
+    <p>Revenue</p>
+    <p>Previous Week</p>
+    <p>Current Week</p>
+    <p>Graph Goes Here</p>
+
+</div>
+<div>
+    <p>Top Selling Products</p>
+    <em>Export Icon</em>
+    <table class="table">
+        <thead>
+            <tr>
+                <th>Product name</th>
+                <th>Date Time</th>
+                <th>Price</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td scope="row">Product 1</td>
+                <td>9 April, 3020</td>
+                <td>$230</td>
+            </tr>
+            <tr>
+                <td scope="row">Product 2</td>
+                <td>8 May, 2930</td>
+                <td>$989</td>
+            </tr>   
+        </tbody>
+    </table>
+</div>
+<div>
+    <p>Total Sales</p>
+    <p>Pie Chart</p>
+</div>
 </template>
 
 <script>
