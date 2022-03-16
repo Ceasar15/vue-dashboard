a070d96e4ad854b597683f9a11f929bc6ee06dd7
Ceasar <ceasarkwadwo@gmail.com>
2022-03-16 12:47:50 +0000

    vuetify card and styles

--- src/views/AdminOrders.vue
+++ src/views/AdminOrders.vue
@@ -7,38 +7,53 @@
           label="Search"
           single-line
           hide-details
+
       ></v-text-field>
     </v-card-title>
     <v-data-table
         :headers="headers"
         :items="desserts"
         :search="search"
+        height="300px"
+
     ></v-data-table>
+    <v-row>
+      <v-col>
+        <v-card>
+          <v-data-table
+              :headers="headers"
+              :items="desserts"
+              :search="search"
+          >
+            ddfd
+          </v-data-table>
+        </v-card>
+      </v-col>
+    </v-row>
   </v-card>
 
 
   <div>
     <div class="flex flex-wrap -mx-1 overflow-hidden">
       <div class="my-1 px-1 w-1/6 overflow-hidden">Column Content</div>
-
       <div class="my-1 px-1 w-1/6 overflow-hidden">Column Content</div>
     </div>
     <div class="flex flex-wrap -mx-1 overflow-hidden">
       <div class="my-1 px-1 w-1/6 overflow-hidden">Column Content</div>
-
       <div class="my-1 px-1 w-1/6 overflow-hidden">Column Content</div>
     </div>
   </div>
 </template>
 
 
-<script>
+<script >
 import { desserts, headers, testData, items } from "@/utils/desserts"
 
 export default {
   name: "AdminOrders",
   data() {
     return {
+      search: '',
       desserts: desserts,
       headers: headers,
       testData: testData,
@@ -47,6 +62,7 @@ export default {
   },
 };
 </script>
-<style scoped>
+<style scoped lang="css">
+@import url("https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css");
 
 </style>
